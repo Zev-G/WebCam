015c9c6b418a953b23e9ad2dd5de7d73bf0096b8
TheMisterWolf <vezvez1234@gmail.com>
2021-11-06 00:09:34 -0700

    Added sliders to control color ranges

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,34 @@
 import com.github.sarxos.webcam.Webcam;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelReader;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import org.controlsfx.control.RangeSlider;
 
 import java.awt.image.BufferedImage;
 import java.util.List;
 
 public class Main extends Application {
+
+    double redMin = 0.3;
+    double redMax = 1;
+    double greenMin = 0.2;
+    double greenMax = 0.4;
+    double blueMin = 0;
+    double blueMax = 0.25;
 
     public static void main(String[] args) {
         launch(args);
@@ -24,11 +37,39 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         final Webcam webcam = Webcam.getDefault();
         webcam.open();
-        final Pane pane = new StackPane();
+
+        Slider minRed = new Slider(0, 1, redMin);
+        Slider maxRed = new Slider(0, 1, redMax);
+        Slider minGreen = new Slider(0, 1, greenMin);
+        Slider maxGreen = new Slider(0, 1, greenMax);
+        Slider minBlue = new Slider(0, 1, blueMin);
+        Slider maxBlue = new Slider(0, 1, blueMax);
+
+        Label ranges = new Label();
+        ranges.textProperty().bind(Bindings.createStringBinding(() ->
+                "Red:   Min = " + minRed.getValue() + "  Max = " + maxRed.getValue() + "\n" +
+                "Green:   Min = " + minGreen.getValue() + "  Max = " + maxGreen.getValue() + "\n" +
+                "Blue:   Min = " + minBlue.getValue() + "  Max = " + maxBlue.getValue() + "\n"
+                , minRed.valueProperty(), maxRed.valueProperty(), minGreen.valueProperty(), maxGreen.valueProperty(), minBlue.valueProperty(), maxBlue.valueProperty()));
+
+        VBox sliders = new VBox(
+                minRed, maxRed, minGreen, maxGreen, minBlue, maxBlue,
+                ranges
+        );
+        sliders.setPrefWidth(450);
+
+        final BorderPane pane = new BorderPane();
+        pane.setRight(sliders);
         Thread thread = new Thread(() -> {
             while (true) {
                 final BufferedImage image = webcam.getImage();
                 Pixel[] allPixels = PixelValidator.loadPixelsFromImage(image);
+                redMin = minRed.getValue();
+                redMax = maxRed.getValue();
+                greenMin = minGreen.getValue();
+                greenMax = maxGreen.getValue();
+                blueMin = minBlue.getValue();
+                blueMax = maxBlue.getValue();
                 Pixel[] validPixels = PixelValidator.validate(
                         allPixels,
                         pixel -> {
@@ -40,9 +81,9 @@ public class Main extends Application {
                             double percentageGreen = color.getGreen() / total;
                             double percentageBlue = color.getBlue() / total;
 
-                            return percentageRed > 0.3 &&
-                                    percentageBlue < 0.25 &&
-                                    percentageGreen > 0.2 && percentageGreen < 0.4 &&
+                            return percentageRed > redMin && percentageRed < redMax &&
+                                    percentageBlue > blueMin && percentageBlue < blueMax &&
+                                    percentageGreen > greenMin && percentageGreen < greenMax &&
                                     averageShade > 0.25;
                         }
                 );
@@ -53,7 +94,7 @@ public class Main extends Application {
                     canvas.setScaleX(6);
                     canvas.setScaleY(6);
                     GraphicsContext context = canvas.getGraphicsContext2D();
-                    pane.getChildren().setAll(canvas);
+                    pane.setCenter(canvas);
 
                     for (Pixel pixel : allPixels) {
                         context.setFill(pixel.getColor());
@@ -63,11 +104,6 @@ public class Main extends Application {
                     for (PixelGroup group : pixelGroups) {
                         List<Pixel> pixels = group.getPixels();
                         if (pixels.size() > 10) {
-                            System.out.println(pixels.size());
-                            for (Pixel pixel : pixels) {
-                                double x = pixel.getX();
-                                double y = pixel.getY();
-                            }
                             context.setFill(Color.rgb(0, 0, 0, 0.5));
                             context.fillRect(group.getMinX(), group.getMinY(), group.getMaxX() - group.getMinX(), group.getMaxY() - group.getMinY());
                         }
